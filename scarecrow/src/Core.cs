c9aff26a5d94927ba2d67aff2d8080e5a5ff192c
Maik Kallenbach <mandikor@gmx.net>
2024-08-10 14:23:29 +0200

    remove unused using directives

--- scarecrow/src/Core.cs
+++ scarecrow/src/Core.cs
@@ -1,18 +1,12 @@
-﻿global using HarmonyLib;
-global using System;
+﻿global using System;
 global using System.Collections.Generic;
 global using System.Linq;
-global using System.Text;
 global using Vintagestory.API.Client;
 global using Vintagestory.API.Common;
 global using Vintagestory.API.Common.Entities;
-global using Vintagestory.API.Config;
-global using Vintagestory.API.Datastructures;
 global using Vintagestory.API.MathTools;
-global using Vintagestory.API.Util;
-global using Vintagestory.Client.NoObf;
-global using Vintagestory.GameContent;
 global using Vintagestory.API.Server;
+global using Vintagestory.API.Util;
 
 namespace Scarecrow
 {
